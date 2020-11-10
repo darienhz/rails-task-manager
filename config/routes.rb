3af6b7950c8c0a35f4080627ec16ce3222c35a20
Darien Hernandez <darienhz89@gmail.com>
2020-11-10 14:35:29 -0600

    did the create

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'tasks#index'
+  # get all tasks
+  get '/tasks', to: 'tasks#index'
+  # get one task
+  get '/tasks/:id', to: 'tasks#show', as: :task
+  # create a task
+  get '/tasks/new', to: 'tasks#new'
+  post '/tasks', to: 'tasks#create'
+  # update task
+  get '/tasks/:id/edit', to: 'tasks#edit'
+  patch '/tasks/:id', to: 'tasks#update'
+  # delete a task
+  delete '/task:id', to: 'tasks#destroy'
 end
